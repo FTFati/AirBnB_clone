6a5724425c5a7e0f6e96304e0db051f9e399b7f3
yasminaa12 <yasminabendrissl@gmail.com>
2023-12-09 08:03:49 -0800

    test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,36 +1,41 @@
 #!/usr/bin/python3
-"""module unit tests for the console.py file"""
-from console import HBNBCommand
+"""
+Contains the class TestConsoleDocs
+"""
+
+import console
+import inspect
+import pep8
 import unittest
-from io import StringIO
+HBNBCommand = console.HBNBCommand
 
 
-class Test_Console(unittest.TestCase):
-    """
-    Test cases for HBNBCommand console (console.py).
-    """
+class TestConsoleDocs(unittest.TestCase):
+    """Class for testing documentation of the console"""
+    def test_pep8_conformance_console(self):
+        """Test that console.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def setUp(self):
-        """Set up test cases"""
-        self.console = HBNBCommand()
-        self.output = StringIO()
+    def test_pep8_conformance_test_console(self):
+        """Test that tests/test_console.py conforms to PEP8."""
+        pep8s = pep8.StyleGuide(quiet=True)
+        result = pep8s.check_files(['tests/test_console.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
 
-    def tearDown(self):
-        """Clean up test cases"""
-        self.output.close()
+    def test_console_module_docstring(self):
+        """Test for the console.py module docstring"""
+        self.assertIsNot(console.__doc__, None,
+                         "console.py needs a docstring")
+        self.assertTrue(len(console.__doc__) >= 1,
+                        "console.py needs a docstring")
 
-    def test_quit(self):
-        """Test the quit command"""
-        self.assertTrue(self.console.onecmd('quit'))
-
-    def test_eof(self):
-        """Test the EOF (ctrl+D) command"""
-        self.assertTrue(self.console.onecmd('EOF'))
-
-    def test_emptyline(self):
-        """Test for the empty line input"""
-        self.assertIsNone(self.console.onecmd(''))
-
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_HBNBCommand_class_docstring(self):
+        """Test for the HBNBCommand class docstring"""
+        self.assertIsNot(HBNBCommand.__doc__, None,
+                         "HBNBCommand class needs a docstring")
+        self.assertTrue(len(HBNBCommand.__doc__) >= 1,
+                        "HBNBCommand class needs a docstring")
